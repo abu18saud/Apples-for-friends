6029837d293a0b0c84ab94af60f3c6551e5477de
Abdullah Alsalem <abdullahalsalem18@gmail.com>
2021-05-04 10:00:10 +0300

    Done

---
+++ Apples for friends/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Apples_for_friends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //your code goes here
+            int applesTom = 3;
+            int applesBob = 2;
+            int applesJames = 4;
+
+            //change the code
+            Console.WriteLine(applesTom);
+            Console.WriteLine(++applesBob);
+            Console.WriteLine(--applesJames);
+        }
+    }
+}
